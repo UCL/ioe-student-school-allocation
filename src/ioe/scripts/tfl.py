056453ad24326d99fa350badadc57ca0b0b6f4c6
Patrick Roddy <patrickjamesroddy@gmail.com>
2023-04-19 16:43:15 +0100

    Add sample data (#2)

--- src/ioe/scripts/tfl.py
+++ src/ioe/scripts/tfl.py
@@ -1,4 +1,5 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
 
 from ioe.data.data_input import read_data
 from ioe.data.data_output import save_output_failures, save_output_journeys
@@ -6,38 +7,52 @@ from ioe.main import compute_all_pairs_journeys
 from ioe.utils.constants import N_CORES
 
 
-def main(subject: str) -> None:
-    """
-    computes the OD matrices for a given set of student school pairs
-    """
-    students = read_data(f"{subject}_students.csv")
-    schools = read_data(f"{subject}_schools.csv")
-    journeys, failures = compute_all_pairs_journeys(
-        subject,
-        students,
-        schools,
-        n_cores=N_CORES,
-    )
-    save_output_journeys(
-        journeys,
-        f"{subject}_student_school_journeys.csv",
-        save_output=True,
-    )
-    save_output_failures(
-        failures,
-        f"{subject}_student_school_failures.csv",
-        save_output=True,
-    )
+def _read_args() -> Namespace:
+    """Read in CLI inputs.
 
-
-if __name__ == "__main__":
+    Returns:
+        The CLI options output.
+    """
     parser = ArgumentParser(
         description=("Constructs a set of student and school data from the main file")
+    )
+    parser.add_argument(
+        "location",
+        type=Path,
+        help="data location",
     )
     parser.add_argument(
         "subject",
         type=str,
         help="placement subject",
     )
-    args = parser.parse_args()
-    main(args.subject)
+    return parser.parse_args()
+
+
+def main() -> None:
+    """
+    computes the OD matrices for a given set of student school pairs
+    """
+    args = _read_args()
+    students = read_data(args.location / f"{args.subject}_students.csv")
+    schools = read_data(args.location / f"{args.subject}_schools.csv")
+    journeys, failures = compute_all_pairs_journeys(
+        args.subject,
+        students,
+        schools,
+        n_cores=N_CORES,
+    )
+    save_output_journeys(
+        journeys,
+        args.location / f"{args.subject}_student_school_journeys.csv",
+        save_output=True,
+    )
+    save_output_failures(
+        failures,
+        args.location / f"{args.subject}_student_school_failures.csv",
+        save_output=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
