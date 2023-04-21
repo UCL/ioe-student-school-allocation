83b58d98f1314b4063c0cdd0e94a2d65645c73a6
Patrick Roddy <patrickjamesroddy@gmail.com>
2023-04-21 17:10:47 +0100

    Fix data to work with data of different format (#8)
    Turns out the example file wasn't representative, so working on IDs
    throughout rather than postcodes.

--- scripts/create_allocation_map.py
+++ scripts/create_allocation_map.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from pathlib import Path
 
 import pandas as pd
@@ -6,7 +7,7 @@ import plotly.express as px
 _file_location = Path(__file__).resolve()
 
 DATA_POSTCODE = "postcode"
-MATCHES_SCHOOL_POSTCODE = "allocation_school_postcode"
+MATCHES_SCHOOL_ID = "allocation_school_id"
 SCHOOL_ID = "SE2 PP: Code"
 SCHOOL_POSTCODE = "SE2 PP: PC"
 SCHOOL_SUFFIX = "_school"
@@ -17,7 +18,7 @@ STUDENT_SUFFIX = "_student"
 
 def _read_data(
     subject: str, postcodes: str
-) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """Reads in the initial school, the matches from `spopt` and the
     UK database on postcodes and prepare the data for processing.
 
@@ -26,39 +27,46 @@ def _read_data(
         postcodes: The name of the postcode data.
 
     Returns:
-        The three prepared dataframes.
+        The four prepared dataframes.
     """
     # prepare whole school data
     schools = pd.read_csv(
         _file_location.parents[1] / "data" / f"{subject}_schools.csv",
         usecols=[SCHOOL_ID, SCHOOL_POSTCODE],
     ).convert_dtypes()
+    # prepare whole student data
+    students = pd.read_csv(
+        _file_location.parents[1] / "data" / f"{subject}_students.csv",
+        usecols=[STUDENT_ID, STUDENT_POSTCODE],
+    ).convert_dtypes()
     # prepare spopt allocated data
     matches = pd.read_csv(
         _file_location.parent / f"{subject}_matches.csv",
         usecols=[
             STUDENT_ID,
-            STUDENT_POSTCODE,
-            MATCHES_SCHOOL_POSTCODE,
+            MATCHES_SCHOOL_ID,
         ],
     ).convert_dtypes()
-    matches[STUDENT_POSTCODE] = matches[STUDENT_POSTCODE].str.replace(" ", "")
     # prepare postcode to lat lon data
     postcodes = pd.read_csv(
         _file_location.parent / f"{postcodes}.csv", usecols=lambda x: x != "id"
     ).convert_dtypes()
     postcodes[DATA_POSTCODE] = postcodes[DATA_POSTCODE].str.replace(" ", "")
-    return schools, matches, postcodes
+    return schools, students, matches, postcodes
 
 
 def _prepare_data(
-    schools: pd.DataFrame, matches: pd.DataFrame, postcodes: pd.DataFrame
+    schools: pd.DataFrame,
+    students: pd.DataFrame,
+    matches: pd.DataFrame,
+    postcodes: pd.DataFrame,
 ) -> pd.DataFrame:
-    """Merges the three dataframes to make a singular dataframe with
+    """Merges the four dataframes to make a singular dataframe with
     student/school ID and the lat lon coordinates.
 
     Args:
         schools: All school data.
+        students: All students data.
         matches: The matched student-school data.
         postcodes: The UK postcode data.
 
@@ -66,11 +74,15 @@ def _prepare_data(
         The prepared dataframe of coordinates.
     """
     # merge all schools on the student matches
-    school_merge_matches = schools.merge(
-        matches, how="left", left_on=SCHOOL_POSTCODE, right_on=MATCHES_SCHOOL_POSTCODE
-    ).drop(columns=MATCHES_SCHOOL_POSTCODE)
+    schools_merge_matches = schools.merge(
+        matches, how="left", left_on=SCHOOL_ID, right_on=MATCHES_SCHOOL_ID
+    ).drop(columns=MATCHES_SCHOOL_ID)
+    # merge students with the composite matches
+    matches_merge_students = schools_merge_matches.merge(
+        students, how="left", on=STUDENT_ID
+    )
     # merge the result with the UK database to get school lat lon coords
-    matches_merge_school_lat_lon = school_merge_matches.merge(
+    matches_merge_school_lat_lon = matches_merge_students.merge(
         postcodes, how="left", left_on=SCHOOL_POSTCODE, right_on=DATA_POSTCODE
     ).drop(columns=[DATA_POSTCODE, SCHOOL_POSTCODE])
     # merge the result with the UK database to get student lat lon coords
@@ -109,6 +121,7 @@ def _prepare_plot(subject: str, df: pd.DataFrame) -> None:
     """Creates the plot of points on a map.
 
     Args:
+        subject: The name of the subject to process.
         df: The prepared datafame.
     """
     # plot all schools
@@ -146,7 +159,7 @@ def _prepare_plot(subject: str, df: pd.DataFrame) -> None:
     fig.show(config={"toImageButtonOptions": {"filename": filename}})
 
 
-def main(*, subject: str, postcodes: str) -> None:
+def main(subject: str, postcodes: str) -> None:
     """Creates a plotly map of all schools and lines
     connecting the matched students.
 
@@ -154,12 +167,23 @@ def main(*, subject: str, postcodes: str) -> None:
         subject: The name of the subject.
         postcodes: The name of the postcode data.
     """
-    schools, matches, postoces = _read_data(subject, postcodes)
-    df = _prepare_data(schools, matches, postoces)
+    schools, students, matches, postoces = _read_data(subject, postcodes)
+    df = _prepare_data(schools, students, matches, postoces)
     _prepare_plot(subject, df)
 
 
 if __name__ == "__main__":
     # UK postcode data taken from
     # https://www.freemaptools.com/download-uk-postcode-lat-lng.htm
-    main(subject="example_subject", postcodes="ukpostcodes")
+    postcode_data = "ukpostcodes"
+    # read in subject
+    parser = ArgumentParser(
+        description=("Creates the visualisation of points on a map per subject")
+    )
+    parser.add_argument(
+        "subject",
+        type=str,
+        help="placement subject",
+    )
+    args = parser.parse_args()
+    main(args.subject, postcode_data)
